57355f38bb75ef83cc20a2e03e84790925d046fa
Stepan Tarabin <upconett@gmail.com>
2025-08-18 13:53:23 +0300

    chore: build.sh modified for new directory structure

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
 
-clang src/main.c -o build/main
+
+INCLUDE_DIR=src
+
+MAIN=src/main.c
+FIELD=src/field/field.c
+RENDER=src/render/render_terminal.c
+
+OUTPUT=build/main
+
+
+clang -I $INCLUDE_DIR  $MAIN $FIELD $RENDER  -o $OUTPUT
